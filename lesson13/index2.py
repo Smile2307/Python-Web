252009e3c1d73a3cd01d65198a14d465f8bd86f7
Smile2307 <101440722+Smile2307@users.noreply.github.com>
2024-06-29 04:01:28 +0000

    modify

--- lesson13/index2.py
+++ lesson13/index2.py
@@ -14,5 +14,19 @@ else:
     data = root.model_dump()
     areas:list[str] = list(set(map(lambda value:value['行政區'],data)))
 
+    #定義callback的function
+    def area_change():
+        st.write(st.session_state.sarea)
+
+    #設定初始值
+    if 'sarea' not in st.session_state:
+        st.session_state.sarea = "淡水區"
+        area_change()
+
+    #設定key值供session使用
     with st.sidebar:
-        st.selectbox(":orange[請選擇行政區域:]",options=areas)
+        st.selectbox(":orange[請選擇行政區域:]",options=areas,on_change=area_change,key='sarea')
+    
+    st.write(st.session_state)
+
+    
