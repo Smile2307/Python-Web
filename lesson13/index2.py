8f615f1e23294be57af14d9a57eefe9717e16afd
Smile2307 <101440722+Smile2307@users.noreply.github.com>
2024-06-29 03:31:20 +0000

    modify

--- lesson13/index2.py
+++ lesson13/index2.py
@@ -1,11 +1,8 @@
 import requests
 from pydantic import BaseModel,Field,RootModel,field_validator,field_serializer
 import streamlit as st
-#匯入modul的function
 import source
-#匯入modul的class
 from source import Root
-
 
 
 try:
@@ -15,11 +12,7 @@ except Exception as e:
 else:
     root = Root.model_validate_json(data_str)
     data = root.model_dump()
+    areas:list[str] = list(set(map(lambda value:value['行政區'],data)))
 
-    def ijk(value):
-        return value['行政區']
-
-    areas:list[str] = list(set(map(ijk,data)))
-
-    option = st.selectbox("請選擇行政區",areas)
-    st.write("您選擇:", option)
+    with st.sidebar:
+        st.selectbox(":orange[請選擇行政區域:]",options=areas)
